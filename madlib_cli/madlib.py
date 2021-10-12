f54828bfe9b0788afa8a89763dfaf0cd545cdc69
shahd1995913 <shahdalkhatib95@gmail.com>
2021-10-12 22:22:54 +0300

    create a function  called a  parse_template

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,3 +1,6 @@
+
+import re
+
 print('Greet to Madlib competition')
 
 """
@@ -9,3 +12,54 @@ def read_template(path):
     fileOpen=open(path)
     fileRead =fileOpen.read()
     return fileRead
+
+"""
+create a function  called a  parse_template
+convert the string to array by return the 2 things 1st part is text and 2ed is  the tuple
+"""
+
+
+def parse_template(input):
+
+    strippd=''
+
+    original_text=[]
+
+    new=input.split(' ')
+
+    print(new)
+
+    regulare=r"^{\w+}|\.$"
+
+    for input1 in new:
+
+        if re.match(regulare,input1)==None :
+
+            strippd+=f"{input1} "
+
+        else :
+
+            if input1==new[-1] :
+
+                strippd+= '{}.' 
+
+                original_text+=[input1[1:-2]]
+
+                
+            else :
+
+                original_text+=[input1[1:-1]]
+
+                strippd+='{} '
+
+    actual_parts=tuple(original_text)
+
+    return (strippd,actual_parts)
+
+
+
+
+
+
+
+
